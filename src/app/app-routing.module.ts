a46e68e1998340f7f57548bcefd7fd7b456cd8b1
pablocm07 <1716110094@utec-tgo.edu.mx>
2020-08-21 17:38:38 -0500

    Cambio de rutas
    Se hizo cambio en el directorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,19 +10,32 @@ const routes: Routes =
   },
   { 
     path: 'home', 
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+    loadChildren: () => import('./home/home-card/home.module').then(m => m.HomeModule) 
   }, 
   { 
     path: 'login-card', 
-    loadChildren: () => import('./login-card/login-card.module').then(m => m.LoginCardModule) 
+    loadChildren: () => import('./login/login-card/login-card.module').then(m => m.LoginCardModule) 
   }, 
   { 
     path: 'password', 
+<<<<<<< Updated upstream
     loadChildren: () => import('./password/password.module').then(m => m.PasswordModule) 
   }, 
   { 
+<<<<<<< Updated upstream
     path: 'register', 
     loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) 
+=======
+    path: 'main', 
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule) 
+=======
+    loadChildren: () => import('./login/password/password.module').then(m => m.PasswordModule) 
+  }, 
+  { 
+    path: 'register', 
+    loadChildren: () => import('./register/register-card/register.module').then(m => m.RegisterModule) 
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,27 @@ import { GetFoodItemsService } from './get-food-items.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginCardComponent } from './login-card/login-card.component';
-import { FoodListComponent } from './food-list/food-list.component';
-import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
-import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginCardComponent } from './login/login-card/login-card.component';
+import { FoodListComponent } from './favorites/food-list/food-list.component';
+import { NavigationMenuComponent } from './main-components/navigation-menu/navigation-menu.component';
+import { FavoritesComponent } from './favorites/favorites-card/favorites.component';
 import { NotificationsComponent } from './notifications/notifications.component';
+<<<<<<< Updated upstream
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { PasswordComponent } from './password/password.component';
+<<<<<<< Updated upstream
 import { RegisterComponent } from './register/register.component';
 
+=======
+import { ReactiveFormsModule } from '@angular/forms';
+=======
+import { HomeComponent } from './home/home-card/home.component';
+import { CartComponent } from './cart/cart-card/cart.component';
+import { PasswordComponent } from './login/password/password.component';
+import { RegisterComponent } from './register/register-card/register.component';
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 const routes: Routes = [
   {path: 'cart', component: CartComponent},
